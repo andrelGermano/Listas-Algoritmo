a130806fc1575cc335d490ed4e817d46324c607c
André Lucas M <127347650+andrelucasmaiagermano@users.noreply.github.com>
2023-05-26 09:40:09 -0300

    Update Q04-L4.cpp

--- Lista 4/Q04-L4.cpp
+++ Lista 4/Q04-L4.cpp
@@ -1,21 +1,25 @@
-/*Elabore um algoritmo para ler um vetor A de 20 números inteiros e obter a maior diferença entre dois 
-elementos consecutivos desse vetor. Ao final, escreva a maior diferença e os índices dos respectivos 
-elementos. */
-
 #include <iostream>
 using namespace std;
-
 int main(){
-	int vet[20], dif=0, i, vet1, vet2;
+	int vet[20], dif[2]={0,0}, i, vet1[2], vet2[2];
 	
 	for(i=0;i<=19;i++){
 		cout << "\nDigite o numero: ";
 		cin >> vet[i];
-		if(vet[i]-(vet[i-1])>dif){
-			dif=vet[i]-vet[i-1];
-			vet1=i;
-			vet2=vet[i-1];
+	}
+	for(i=0;i<=19;i++){
+		if(vet[i]<vet[i+1]){
+			dif[0]=(vet[i]-vet[i+1])*-1;
+		}else{
+			dif[0]=vet[i]-vet[i+1];
+		}
+		if(dif[0]>dif[1]){
+			vet1[0]=i;
+			vet2[0]=i+1;
+			vet1[1]=vet[i];
+			vet2[1]=vet[i+1];
+			dif[1]=dif[0];
 		}
 	}
-	cout << "\nMaior diferença: " << "indice " << vet1 << " - " << "indice " << vet2 << " = " << dif;
+	cout << "\nMaior diferença: " << "indice " << vet1[0] << "(" << vet1[1] << ") - indice " << vet2[0] << "(" << vet2[1] << ")" << "= " << dif[1];
 }
