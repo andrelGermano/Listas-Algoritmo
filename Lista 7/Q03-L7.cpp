85beb1481d287ecc2b5171e0346804308ad42843
André Lucas M <127347650+andrelGermano@users.noreply.github.com>
2023-06-25 14:30:34 -0300

    Update Q03-L7.cpp

--- Lista 7/Q03-L7.cpp
+++ Lista 7/Q03-L7.cpp
@@ -1,5 +1,5 @@
-/*3. Escreva uma função que recebe 2 números inteiros n1 e n2 como entrada e retorna a soma de todos os números inteiros contidos no 
-intervalo [n1,n2]. Use esta função em um programa que lê n1 e n2 do usuário e imprime a soma. */
+/*3. Escreva uma funÃ§Ã£o que recebe 2 nÃºmeros inteiros n1 e n2 como entrada e retorna a soma de todos os nÃºmeros inteiros contidos no 
+intervalo [n1,n2]. Use esta funÃ§Ã£o em um programa que lÃª n1 e n2 do usuÃ¡rio e imprime a soma. */
 #include <iostream>
 using namespace std;
 
@@ -14,7 +14,7 @@ int main(){
 	cout << "\n soma de todos os numeros inteiros contidos no intervalo [n1,n2]: " << somaIntervalo(n1+1, n2);
 }
 int somaIntervalo (int num1, int num2){
-	int i, soma=0;
+	int soma=0;
 	do{
 		soma+=num1;
 		num1++;
