9c28c1c024b3b02bfd028b86d4d139ef66edc5e1
André Lucas M <127347650+andrelGermano@users.noreply.github.com>
2023-06-27 22:38:54 -0300

    Update Q06-L7.cpp

--- Lista 7/Q06-L7.cpp
+++ Lista 7/Q06-L7.cpp
@@ -1,28 +1,39 @@
-/*6.)Elaborar um algoritmo que leia um vetor de 10 posições de inteiros e chame uma função MinMax. Esta função deve receber o vetor lido e, por referência, duas variáveis inteiras, min e max. O objetivo da função é buscar os valores do menor e maior elementos do vetor e atribuir 
-às variáveis min e max respectivamente.*/
 #include <iostream>
 using namespace std;
-int i;
-const int tam = 5;
-void MinMax(int &min, int &max, int v[]);
+
+void minMax(int *v, int *pMin, int *pMax);
+
+
 int main(){
-	int v[tam], min, max;
-	for(i=0;i<tam;i++){
-		cout << "\nNumero [" << i+1 << "]: ";
-		cin >> v[i];
-	}
-	MinMax(min, max, v);
-	cout << "\nMenor: " << min;
-	cout << "\nMaior: " << max;
-}
-void MinMax(int &min, int &max, int v[]){
-	min=v[0];
-	max=v[0];
-	for(i=0;i<tam;i++){
-		if(min>v[i])
-			min=v[i];
-		if(max<v[i])
-			max=v[i];
-	}
+    int vetor[10], i, min, max;
+
+    for(i=0; i<10; i++){
+        cout << "\n\tDigite o elemento vetor[" << i << "]: ";
+        cin >>vetor[i];
+    }
+
+    minMax(vetor, &min, &max);
+
+    cout << "\n\tMenor elemento: " << min;
+    cout << "\n\tMaior elemento: " << max;
+
+    cout << "\n\tFim de programa";
+    system("PAUSE");
+
 }
 
+void minMax(int *v, int *pMin, int *pMax){
+    int i, menor, maior;
+    menor = maior = v[0];
+    for(i=1; i<10; i++){
+        if(v[i] < menor){
+            menor = v[i];
+        }
+        if(v[i]>maior){
+            maior = v[i];
+        }
+    }
+    *pMin = menor;
+    *pMax = maior;
+
+}
